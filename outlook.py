9b40425e6bd1879f2e459a5d78c0aab86b172fe5
dtehan <daniel_tehan@yahoo.com.au>
2016-02-23 09:41:17 -0800

    initial load

---
+++ outlook.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 30 15:17:31 2015
+
+@author: DT310750
+"""
+
+import string
+import win32com.client
+
+
+#Get Lastt message body
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
+                                    # the inbox. You can change that number to reference
+                                    # any other folder
+messages = inbox.Items
+message = messages.GetLast()
+body_content = message.body
+
+counts = dict()
+text = body_content
+text = text.strip(string.punctuation)
+text = text.strip(string.digits)
+text = text.strip()
+text = text.lower()
+words = text.split()
+for word in words:
+    if len(word) < 4 : continue
+    counts[word] = counts.get(word,0) + 1
+
+x = sorted(counts, key=counts.get, reverse=True)
+highest = None
+lowest = None
+for k in x[:100]:
+    if highest is None or highest < counts[k] :
+        highest = counts[k]
+    if lowest is None or lowest > counts[k] :
+        lowest = counts[k]
+print( 'Range of counts:',highest,lowest)
+
+# Spread the font sizes across 20-100 based on the count
+bigsize = 80
+smallsize = 20
+
+fhand = open('gword.js','w')
+fhand.write("gword = [")
+first = True
+for k in x[:100]:
+    if not first : fhand.write( ",\n")
+    first = False
+    size = counts[k]
+    size = (size - lowest) / float(highest - lowest)
+    size = int((size * bigsize) + smallsize)
+    fhand.write("{text: '"+k+"', size: "+str(size)+"}")
+fhand.write( "\n];\n")
+
+print( "Output written to gword.js")
